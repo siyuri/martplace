b797dc5ec16877b8447ba0c5fbdbd3909189a88f
siyuri <siyuri0907@mail.ru>
2020-07-07 19:12:48 +0300

    working top slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
-$(function(){
-
-
-
-  
-});
+$(function () {
+  $('.slider-featured__products').slick({
+    infinite: true,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/previous.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.png" alt=""></button>'
+  })
+})
