927b7fabd4cde0aab6841941074f3234e1d36cfc
siyuri <siyuri0907@mail.ru>
2020-07-08 19:54:07 +0300

    working mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,6 @@ $(function () {
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/previous.png" alt=""></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/next.png" alt=""></button>'
   })
+
+  var mixer = mixitup('.newest-products__items')
 })
