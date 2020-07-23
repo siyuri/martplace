f32b95b63e5d88b853e7e7e760b47e0c84549a94
siyuri <siyuri0907@mail.ru>
2020-07-24 00:42:10 +0300

    adaptive footer,sliders

--- app/js/main.js
+++ app/js/main.js
@@ -18,7 +18,7 @@ $(function () {
     nextArrow: '<button type="button" class="slick-btn slick-next"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
     appendArrows: $('.follow-slider__title'),
     responsive: [{
-      breakpoint: 1066,
+      breakpoint: 1150,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2
@@ -26,14 +26,42 @@ $(function () {
     }]
   })
 
+  var slider = $('.follow-slider__items');
+
+  $('.follow-slider__title .slick-prev').on('click', function() {
+    $(slider).slick('slickPrev');
+  });
+  $('.follow-slider__title .slick-next').on('click', function() {
+    $(slider).slick('slickNext');
+  });
+
+
   $('.feedback__cards').slick({
     arrows: true,
     slidesToShow: 2,
     slidesToScroll: 2,
     prevArrow: '<button type="button" class="slick-btn slick-prev-feedback"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-left"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next-feedback"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>'
-
+    nextArrow: '<button type="button" class="slick-btn slick-next-feedback"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
+    responsive: [{
+      breakpoint: 1065,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows:false
+      }
+    }]
   })
+
+  $('.header').on('click', '.search-toggle', function(e) {
+    var selector = $(this).data('selector');
+  
+    $(selector).toggleClass('show').find('.search-form__input').focus();
+    $(this).toggleClass('active');
+  
+    e.preventDefault();
+  });
 
   // raty
   $('.star-rating').raty({
